fdc444ff53cccade111f98d0c237d9a8a6125aaf
ittp <tp@ittps.ru>
2022-07-25 20:29:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-txnkcb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-txnkcb)
---
+++ index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './demo';
+import Sidebar from '@mui/material/Sidebar';
+import { BottomNavigation } from '@mui/material';
+ReactDOM.createRoot(document.querySelector('#root')).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <Demo />
+      <BottomNavigation />
+      <div></div>
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
